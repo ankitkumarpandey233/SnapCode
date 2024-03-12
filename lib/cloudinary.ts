5be42160395e3e05dfca4f573ea0f1b2bc266648
Ankit <ankitpandey21@gnu.ac.in>
2024-03-13 01:03:54 +0530

    Refactor image upload function to handle errors

--- lib/cloudinary.ts
+++ lib/cloudinary.ts
@@ -2,19 +2,25 @@ import { env } from "~/env.mjs"
 
 export const upload = async (base64: string) => {
   if (base64) {
-    const imageRes = await fetch(
-      `https://api.cloudinary.com/v1_1/${env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/upload`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          upload_preset: env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET,
-          file: base64,
-        }),
-      }
-    ).catch(error => console.error('Error:', error));
+    let imageRes: Response | void;
+    try {
+      imageRes = await fetch(
+        `https://api.cloudinary.com/v1_1/${env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/upload`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            upload_preset: env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET,
+            file: base64,
+          }),
+        }
+      );
+    } catch (error) {
+      console.error('Error:', error);
+      return null;
+    }
 
     if (!imageRes.ok) {
       throw new Error(`HTTP error: ${imageRes.status} ${imageRes.statusText}`)
